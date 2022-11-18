39ba00865a56a1782d68900c86f3a1632806e549
Renzo-Henrique <renzolealguzzo@gmail.com.br>
2022-11-17 21:01:13 -0300

    finalizado com print de tempo

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]){
     assertx( !(argc <= 1) , "Insira parametros de leitura e escrita");
     assertx( !(argc <= 2) , "Insira parametros de escrita");
 
-    double start = clock();
+    double start_total = clock();
 
     int V, E, S, C, M;
     Graph* g;
@@ -41,11 +41,12 @@ int main(int argc, char* argv[]){
 
     //doing Dijkstra
     graph_TOTAL_Dijkstra(g);
+    double end_dijkstra = clock();
 
     FILE* output_file = file_open(argv[2], "w");
-    
+    double start_rtt = clock();
     Inflation* vector_inflation = rtt_calculate_inflation(graph_get_minimum_paths(g), graph_get_qtd(g), S, C, M, server, client, monitor);
-
+    double end_rtt = clock();
     /* ---- Trying to free memory to improve execution time of rtt_sort
     */
     graph_free(g);
@@ -58,6 +59,10 @@ int main(int argc, char* argv[]){
     /* Trying to sort the structs of inflation to print in the exit file
     */
     //printf("Trying to sort the inflation\n");
+    
+
+    double start_sort = clock();
+
     int size_vector = S * C;
     rtt_sort(vector_inflation, size_vector);
     rtt_print(vector_inflation, size_vector, output_file);
@@ -65,9 +70,12 @@ int main(int argc, char* argv[]){
     rtt_free(vector_inflation);
     file_close(output_file);
 
-    double end = clock();
+    double end_total = clock();
 
-    printf("Execution time is: %lf\n", (end - start)/CLOCKS_PER_SEC);
+    printf("Djikstra execution time is: %lf\n", (end_dijkstra - start_total)/CLOCKS_PER_SEC);
+    printf("Inflation calculation execution time is: %lf\n", (end_rtt - start_rtt)/CLOCKS_PER_SEC);
+    printf("Sort execution time is: %lf\n", (end_total - start_sort)/CLOCKS_PER_SEC);
+    printf("Total execution time is: %lf\n\n", (end_total - start_total)/CLOCKS_PER_SEC);
 
     
     return 0;
